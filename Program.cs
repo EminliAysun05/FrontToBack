b176d57c530dd6f0f039e5497a5a4348844b97ae
EminliAysun05 <aeminli232@gmail.com>
2024-09-12 16:33:52 +0400

    task is done

---
+++ Program.cs
@@ -0,0 +1,29 @@
+namespace FrontToBack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            //  builder.Services.AddMvc();
+            var app = builder.Build();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}"));
+
+            //app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id}");
+            //app.MapGet("/", () => "Hello World!");
+
+            app.UseStaticFiles();
+            //Ctrl+Shift+B
+
+
+            //        app.MapControllerRoute(
+            //name: "default",
+            //pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
